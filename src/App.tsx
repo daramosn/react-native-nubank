51aa52fa7a83aec8b235a9141b942ea7610bcc21
daramosn <daramosn@unal.edu.co>
2025-03-16 17:26:27 -0500

    chore: add paper provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+/* eslint-disable react/no-unstable-nested-components */
 import '../gesture-handler';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {StatusBar} from 'react-native';
 import {StackNavigator} from './routes/StackNavigator';
+import {PaperProvider} from 'react-native-paper';
+import Ionicon from 'react-native-vector-icons/Ionicons';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <StatusBar barStyle="light-content" backgroundColor="#1e1e1e" />
-      <StackNavigator />
+      <PaperProvider settings={{icon: props => <Ionicon {...props} />}}>
+        <StatusBar barStyle="light-content" backgroundColor="#1e1e1e" />
+        <StackNavigator />
+      </PaperProvider>
     </NavigationContainer>
   );
 };
